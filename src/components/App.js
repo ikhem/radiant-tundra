8a7bf84630cc13ff14237f60e7dfd8609d76ebdd
Im Khem <im.khem@gmail.com>
2017-11-11 01:59:57 -0800

    router implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import Popular from './Popular';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import '../index.css';
+import Nav from './Nav';
+import Home from './Home';
+import Battle from './Battle';
 
 class App extends Component {
   render() {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route render={()=>{return <p>Not Found</p>}} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
+
+const Nav = () => {
+  return (
+    <div>
+      <ul className='nav'>
+        <li>
+          <NavLink exact activeClassName='active' to='/'>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName='active' to='/battle'>
+            Battle
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName='active' to='/popular'>
+            Popular
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
